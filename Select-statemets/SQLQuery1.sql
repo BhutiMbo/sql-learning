1eecb35d8137a651d3d5fe4da5968ff2a02a3d9f
Mbongeni Khumalo <mmbongenik@gmail.com>
2025-10-08 07:02:30 +0200

    update SQL Learning files and readme

--- Select-statemets/SQLQuery1.sql
+++ Select-statemets/SQLQuery1.sql
@@ -1,1 +1,18 @@
 select 'Mbongeni', 'Genius', 'Khumalo', 'Kasenzangakhona'
+select 1+1, 5-3, 4*4, 4/4
+
+Select top 2 *
+from Person.Person
+
+SELECT TOP 5 FirstName, MiddleName, LastName
+From Person.person
+
+Select TOP 5 FirstName as [Customer First Name], LastName as "Customer Last Name"
+from Person.person
+
+Select top 2 *
+from HumanResources.vEmployee
+
+Select top 2 *
+from HumanResources.Employee
+
