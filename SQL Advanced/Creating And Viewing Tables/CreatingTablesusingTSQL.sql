defff228141d5c5939ab32d96956f28a95fd5e80
Mbongeni Khumalo <mmbongenik@gmail.com>
2025-11-15 07:31:57 +0200

    Re-upload SQL Advanced folder via Git

---
+++ SQL Advanced/Creating And Viewing Tables/CreatingTablesusingTSQL.sql
@@ -0,0 +1,10 @@
+create table tblSecond
+(myNumbers int);
+
+insert into tblSecond
+values(234)
+
+insert into tblSecond values(678)
+
+insert into [dbo].[tblSecond] values (456),(156),(467)
+
---
+++ SQL Advanced/Creating And Viewing Tables/ViewingTables.sql
@@ -0,0 +1,8 @@
+select *
+from [tblSecond]
+
+select myNumbers
+from dbo.tblSecond
+
+select *
+from [tblFirst]
